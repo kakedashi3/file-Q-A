d325d4a1bd05af3f299ce220eaa0b12b79b8a5e2
kakedashi3 <riki_funk_x@yahoo.co.jp>
2023-04-28 17:43:07 +0900

    first commit

---
+++ nextjs/src/pages/index.tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import { useState } from "react";
+
+import FileQandAArea from "../components/FileQandAArea";
+import { FileLite } from "../types/file";
+import FileUploadArea from "../components/FileUploadArea";
+
+export default function FileQandA() {
+  const [files, setFiles] = useState<FileLite[]>([]);
+
+  return (
+    <div className="flex items-left text-left h-screen flex-col">
+      <Head>
+        <title>File Q&A</title>
+      </Head>
+      <div className="max-w-3xl mx-auto m-8 space-y-8 text-gray-800">
+        <h1 className="text-4xl">File Q&A</h1>
+
+        <div className="">
+        ファイル内のコンテンツから回答を検索するには、
+        ここにアップロードしてください。OpenAI の埋め込みと GPT を使用して、
+        関連するドキュメントから回答を見つけます。
+        </div>
+
+        <FileUploadArea
+          handleSetFiles={setFiles}
+          maxNumFiles={75}
+          maxFileSizeMB={30}
+        />
+
+        <FileQandAArea files={files} />
+      </div>
+    </div>
+  );
+}
